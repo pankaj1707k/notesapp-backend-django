4dcc2d7382150347fc593a81a7ecdc45328fbf0c
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-12 19:38:46 +0530

    feat: add Profile model and set related config Include - directory and URL setting for media, migration file

--- base/settings.py
+++ base/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media files (Images, Audio, Video)
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Model to represent the profile of a user.
+    It holds the non-authentication related additional data of a user.
+    It has a one-to-one relationship with the User model.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(_("phone number"), max_length=16, blank=True)
+    avatar = models.ImageField(
+        _("avatar"), default="avatars/default.png", upload_to="avatars"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} profile"
