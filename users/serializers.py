250abe75c1afcb9b9291bf6a07251d4498cead0f
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-12 23:45:56 +0530

    feat: validate password during user registration Enforce django's default password validators during deserialization

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -13,3 +14,13 @@ class UserCreateSerializer(serializers.ModelSerializer):
             "email": {"validators": [UniqueValidator(User.objects.all())]},
             "password": {"write_only": True},
         }
+
+    def validate(self, attrs):
+        """
+        Validate POST data during deserialization
+        """
+
+        # Run django's default password validators
+        validate_password(attrs["password"])
+
+        return super().validate(attrs)
