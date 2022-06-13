3c308fbe99e6d83978689e93e5d6c4e3a574676a
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-13 18:19:42 +0530

    config: set default permission class

--- base/settings.py
+++ base/settings.py
@@ -137,5 +137,6 @@ MEDIA_URL = "/media/"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
