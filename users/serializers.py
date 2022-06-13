ba3330c07cc3c10511dbd50329c6f131b9b3fe40
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-13 17:16:53 +0530

    refactor: mod user create response in serializer Move the logic to include token in response of user creation from view to serializer

--- users/serializers.py
+++ users/serializers.py
@@ -11,9 +11,11 @@ class UserCreateSerializer(serializers.ModelSerializer):
     Serializer to handle creation of User instance
     """
 
+    token = serializers.CharField(source="auth_token.key", read_only=True)
+
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "email", "password", "token"]
         extra_kwargs = {
             "email": {"validators": [UniqueValidator(User.objects.all())]},
             "password": {"write_only": True},
--- users/views.py
+++ users/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.status import *
 
 from .serializers import UserCreateSerializer
@@ -17,18 +15,3 @@ class CreateUser(CreateAPIView):
 
     permission_classes = [AllowAny]
     serializer_class = UserCreateSerializer
-
-    def create(self, request, *args, **kwargs):
-        """
-        Method to create a User instance.
-        Overridden to generate custom response.
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-
-        # Send token of the new user in response along with other data
-        user_data = dict(serializer.data)
-        token = Token.objects.get(user__username=user_data["username"]).key
-        user_data["token"] = token
-        return Response(user_data, HTTP_201_CREATED)
