ba2811964e6a361feaad5f5c940e13fb9593beb5
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-13 16:54:11 +0530

    feat: auto-create auth token on user registration

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 from .models import Profile
 
@@ -12,3 +13,10 @@ def create_profile(instance, created, *args, **kwargs):
     """Signal to auto-create a profile for newly created user"""
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def create_token(instance, created, *args, **kwargs):
+    """Signal to create auth token for newly created user"""
+    if created:
+        Token.objects.create(user=instance)
